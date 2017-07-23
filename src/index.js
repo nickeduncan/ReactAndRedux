c5bd066774317c376638b77c66977d3e50596b71
Nicholas Duncan <nickeduncan@gmail.com>
2017-07-23 16:45:02 -0400

    [1-11] rendered the component in DOM

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM form 'react-dom';
+import ReactDOM from 'react-dom';
 
 // Create a new component. This component should produce
 // some HTML
-const App = function() {
+const App = () => {
 	return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML an dput it
 // on the page (in the DOM)
-ReactDOM.render(<App />);
+ReactDOM.render(<App />, document.querySelector('.container'));
