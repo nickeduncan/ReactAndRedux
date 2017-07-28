07028547c205d4701dda3bf1b44230fafad7bd71
Nicholas Duncan <nickeduncan@gmail.com>
2017-07-27 21:14:24 -0400

    [1-18] used set state on searchbar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 	constructor(props) {
-	super(props);
+		super(props);
 
-	this.state = { term: '' };
+		this.state = { term: '' };
 	}
 
 	render() {
-		return (<input onChange={(event) => console.log(event.target.value)} />);
+		return (
+			<div>
+				<input onChange={event => this.setState({ term: event.target.value })} />
+			</div>
+		);
 	}
 }
 
