935b1418b7fb6657e66b89feffa8e99d5aa70de3
Nicholas Duncan <nickeduncan@gmail.com>
2017-07-28 21:01:57 -0400

    [1-22] added state to App

--- src/index.js
+++ src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
 
 const API_KEY="AIzaSyAOn32a__11gFOcuRpBdF81tBoGB8SuYCc"
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-	console.log(data);
-});
 
-// Create a new component. This component should produce
-// some HTML
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+const App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>  {
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		);
+	}
 }
 
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
