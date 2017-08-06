9b8d93b5871dbe673862b61549cb00cc6a519aa8
Nicholas Duncan <nickeduncan@gmail.com>
2017-08-06 13:29:31 -0400

    [2-42] connected books reducer to book-list container

--- project2_redux/src/containers/book-list.js
+++ project2_redux/src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class Booklist extends Component {
+class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
@@ -17,3 +18,13 @@ export default class Booklist extends Component {
 		)
 	}
 }
+
+function mapStateToProps(state) {
+	// Whatever is returned will shows up as props
+	// inside of BookList
+	return {
+		books: state.books
+	};
+}
+
+export default connect(mapStateToProps)(BookList);
