0d74f6b304d0593193bcb72e170e7ad0bd41917e
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-02 16:18:32 -0400

    [2-43] ActionCreator is called by click on book-list and returns payload

--- project2_redux/src/containers/book-list.js
+++ project2_redux/src/containers/book-list.js
@@ -7,7 +7,12 @@ class BookList extends Component {
 	renderList() {
 		return this.props.books.map((book) => {
 			return (
-				<li key={book.title} className="list-group-item">{book.title}</li>
+				<li
+					key={book.title}
+					onClick={() => this.props.selectBook(book)}
+					className="list-group-item">
+					{book.title}
+				</li>
 			);
 		});
 	}
@@ -34,7 +39,7 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
 	// Whenever selectBook is called, the result should be passed
 	// to all of our reducers
-	return bindActionCreators({ selectBook: selectBook }, dispatch)
+	return bindActionCreators({ selectBook: selectBook }, dispatch);
 }
 
 // Promote BookList from a component to a container - it needs to know
