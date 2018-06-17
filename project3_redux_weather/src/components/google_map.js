47adc4ee65e947103934c7e6eb1081ff7aee7156
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-17 15:57:02 -0400

    [5-88] added google map under city name

--- project3_redux_weather/src/components/google_map.js
+++ project3_redux_weather/src/components/google_map.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 
 class GoogleMap extends Component {
 	componentDidMount() {
-		new goolge.maps.Map(this.refs.map, {
+		new google.maps.Map(this.refs.map, {
 			zoom: 12,
 			center: {
 				lat: this.props.lat,
 				lng: this.props.lon
 			}
-		}
+		});
 	}
 	
 	render() {
