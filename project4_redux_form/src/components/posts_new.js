96a561c019a5f16f849f4a9b7003bcef5bb23df1
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-18 20:47:38 -0400

    [9-136] added validations for new post

--- project4_redux_form/src/components/posts_new.js
+++ project4_redux_form/src/components/posts_new.js
@@ -24,8 +24,8 @@ class PostsNew extends Component {
 			    component={this.renderField}
 			  />
 			  <Field
-			    label="Tags"
-			    name="tags"
+			    label="Categories"
+			    name="categories"
 			    component={this.renderField}
 			  />
 			  <Field
@@ -38,6 +38,28 @@ class PostsNew extends Component {
 	}
 }
 
+function validate(values) {
+  const errors = {};
+
+	// Validate the inputs from 'values'
+	if (!values.title) {
+		errors.title = "Enter a title";
+	}
+
+	if (!values.categories) {
+		errors.title = "Enter some categories";
+	}
+
+	if (!values.content) {
+		errors.title = "Enter some content please!";
+	}
+
+  // If errors is empty the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is valid
+	return errors;
+}
+
 export default reduxForm({
+	validate,
   form: 'PostsNewForm'
 })(PostsNew);
