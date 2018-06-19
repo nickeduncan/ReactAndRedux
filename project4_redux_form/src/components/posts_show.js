b8fd257bd60888cb26ff0561627e67a9f45b2660
Nicholas Duncan <nickeduncan@gmail.com>
2018-06-19 10:45:10 -0400

    [9-147] render post on show post and added loading screen while component mounts

--- project4_redux_form/src/components/posts_show.js
+++ project4_redux_form/src/components/posts_show.js
@@ -4,14 +4,22 @@ import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
 	componentDidMount() {
-		const { id } = this.props.match.params.id;
+		const { id } = this.props.match.params;
 		this.props.fetchPost(id);
 	}
 
 	render() {
+		const { post } = this.props;
+
+		if (!post) {
+			return <div>Loading...</div>
+		}
+
 		return (
 			<div>
-				Posts Show!
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
 			</div>
 		);
 	}
